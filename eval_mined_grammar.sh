7f7313bcdeb0cf46ff1d54f7f65db9360fa6f8d4
Caroline Lemieux <caro.lemieux@gmail.com>
2021-03-11 18:07:17 -0800

    pritn out oracle calls

--- eval_mined_grammar.sh
+++ eval_mined_grammar.sh
@@ -10,4 +10,5 @@ recall=$(python3 eval_larkgram.py ${bench}-gram.lark examples/${bench}/test_set/
 precision=$(tail -n 1 ${bench}-eval.log | sed 's/PASS RATE://' )
 runtime=$(tail -n 1 ${bench}-learn.log | awk '{print $3}')
 f1=$(echo $recall $precision | awk '{print 2*$1*$2/($1+$2)}')
-printf " %.2f & %.2f & %.2f & %.0fs \n" $recall $precision $f1 $runtime > ${bench}-results.asv
+oracles=$(grep ORACLE ${bench}-learn.log | awk '{s += $3} END {print s}')
+printf " %.2f & %.2f & %.2f & %.0fs & %i \n" $recall $precision $f1 $runtime $oracles > ${bench}-results.asv
