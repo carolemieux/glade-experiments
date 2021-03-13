d4ce83b38b4874248079c7ae35e95a0e764c9dbc
Caroline Lemieux <caro.lemieux@gmail.com>
2021-03-13 12:10:41 -0800

    a check

--- eval_mined_grammar.sh
+++ eval_mined_grammar.sh
@@ -2,10 +2,15 @@
 
 bench=$1
 
-egrep "GRAMMAR|MERGE" glade-results/${bench}-eval.log > raw-gram.tmp
-python3 larkfixup.py raw-gram.tmp > glade-results/${bench}-gram.lark
+if [ -z "$PYTHON" ]; then
+  echo "Please set \$PYTHON to the location of the python you want me to use (maybe a pypy? :) )"
+  exit 1
+fi
 
-recall=$(python3 eval_larkgram.py glade-results/${bench}-gram.lark examples/${bench}/test_set/* | tail -n 1 | awk '{print $2}' )
+egrep "GRAMMAR|MERGE" glade-results/${bench}-eval.log > raw-gram.tmp
+$PYTHON larkfixup.py raw-gram.tmp > glade-results/${bench}-gram.lark
+
+recall=$($PYTHON eval_larkgram.py glade-results/${bench}-gram.lark examples/${bench}/test_set/* | tail -n 1 | awk '{print $2}' )
 precision=$(tail -n 1 glade-results/${bench}-eval.log | sed 's/PASS RATE://' )
 runtime=$(tail -n 1 glade-results/${bench}-learn.log | awk '{print $3}')
 f1=$(echo $recall $precision | awk '{print 2*$1*$2/($1+$2)}')
