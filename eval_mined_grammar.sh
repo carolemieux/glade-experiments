dc59b31d3e762cb920aa4e0bf1e6d5d17980da98
Caroline Lemieux <caro.lemieux@gmail.com>
2021-03-13 12:30:58 -0800

    keep a log

--- eval_mined_grammar.sh
+++ eval_mined_grammar.sh
@@ -10,7 +10,9 @@ fi
 egrep "GRAMMAR|MERGE" glade-results/${bench}-eval.log > raw-gram.tmp
 $PYTHON larkfixup.py raw-gram.tmp > glade-results/${bench}-gram.lark
 
-recall=$($PYTHON eval_larkgram.py glade-results/${bench}-gram.lark examples/${bench}/test_set/* | tail -n 1 | awk '{print $2}' )
+$PYTHON eval_larkgram.py glade-results/${bench}-gram.lark examples/${bench}/test_set/* > glade-results/${bench}-recall.log
+
+recall=$(tail -n 1 glade-results/${bench}-recall.log | awk '{print $2}' )
 precision=$(tail -n 1 glade-results/${bench}-eval.log | sed 's/PASS RATE://' )
 runtime=$(tail -n 1 glade-results/${bench}-learn.log | awk '{print $3}')
 f1=$(echo $recall $precision | awk '{print 2*$1*$2/($1+$2)}')
